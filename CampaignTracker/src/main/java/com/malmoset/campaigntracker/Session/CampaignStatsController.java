34bad0f2f44f9ac1531ca5de39315ce1ef60e7a1
Sean Mollet <sean@malmoset.com>
2018-03-11 11:52:27 -0500

    Stats works

--- CampaignTracker/src/main/java/com/malmoset/campaigntracker/Session/CampaignStatsController.java
+++ CampaignTracker/src/main/java/com/malmoset/campaigntracker/Session/CampaignStatsController.java
@@ -11,8 +11,10 @@ import com.malmoset.campaigndata.XPEvent;
 import com.malmoset.campaigntracker.MainApp;
 import com.malmoset.controls.BaseForm;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -21,6 +23,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -48,8 +52,16 @@ public class CampaignStatsController extends BaseForm implements Initializable {
             return Integer.compare(left.getBattleNumber(), right.getBattleNumber());
         });
         Map<Integer, ObservableList<XPEvent>> xpEntries = MainApp.getAppData().getDb().getXP();
+        Map<Integer, List<Battle>> sessionBattles = battles.stream().collect(Collectors.groupingBy(x -> x.getSession()));
+        List<XPEvent> allXPevents = xpEntries.values().stream().flatMap(List::stream).collect(Collectors.toList());
+        Map<String, Long> monsterKills = allXPevents.stream().collect(Collectors.groupingBy(XPEvent::getMonster, Collectors.counting()));
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+
+        AppendText("", Color.CORNFLOWERBLUE, true);
 
         AppendText("Campaign Manager Campaign " + MainApp.getAppData().getDb().getCampaignName(), Color.MEDIUMVIOLETRED, true);
+        AppendText("", Color.CORNFLOWERBLUE, true);
 
         if (battles.size() > 0) {
 
@@ -62,44 +74,46 @@ public class CampaignStatsController extends BaseForm implements Initializable {
         AppendText("Heroes:", Color.GREENYELLOW, true);
 
         for (Player player : MainApp.getAppData().getDb().getPlayers()) {
-            AppendText(player.getName() + " the " + player.getRace() + " " + player.getCharacter_class(), Color.GREENYELLOW, true);
+            if (player.getRace() != null && player.getRace().length() > 0) {
+                AppendText(player.getName() + " the " + player.getRace() + " " + player.getCharacter_class(), Color.GREENYELLOW, true);
+            }
         }
 
         AppendText("", Color.CORNFLOWERBLUE, true);
         int totalmonsters = battles.stream().collect(Collectors.summingInt(x -> x.getTotalMonsters()));
 
-        AppendText("Monsters killed: " + battles.stream().collect(Collectors.summingInt(x -> x.getDeadMonsters())).toString(), Color.CORNFLOWERBLUE, true);
-        AppendText("Monsters not killed (XP Given): " + battles.stream().collect(Collectors.summingInt(x -> x.getPersuadedMonsters())).toString(), Color.CORNFLOWERBLUE, true);
-        AppendText("Monsters not killed (no XP Given): " + battles.stream().collect(Collectors.summingInt(x -> x.getUntouchedMonsters())).toString(), Color.CORNFLOWERBLUE, true);
-        AppendText("XP Earned: " + battles.stream().collect(Collectors.summingInt(x -> x.getTotalXP())).toString(), Color.CORNFLOWERBLUE, true);
+        AppendText("Monsters killed: " + numberFormat.format(battles.stream().collect(Collectors.summingInt(x -> x.getDeadMonsters()))), Color.CORNFLOWERBLUE, true);
+        AppendText("Monsters not killed (XP Given): " + numberFormat.format(battles.stream().collect(Collectors.summingInt(x -> x.getPersuadedMonsters()))), Color.CORNFLOWERBLUE, true);
+        AppendText("Monsters not killed (no XP Given): " + numberFormat.format(battles.stream().collect(Collectors.summingInt(x -> x.getUntouchedMonsters()))), Color.CORNFLOWERBLUE, true);
+        AppendText("XP Earned: " + numberFormat.format(allXPevents.stream().collect(Collectors.summingInt(x -> x.getXP()))), Color.CORNFLOWERBLUE, true);
 
         AppendText("", Color.CORNFLOWERBLUE, true);
 
         AppendText("Stats by session:", Color.CORNFLOWERBLUE, true);
-        Map<Integer, List<Battle>> sessions = battles.stream().collect(Collectors.groupingBy(x -> x.getSession()));
-        for (Map.Entry<Integer, List<Battle>> battle : sessions.entrySet()) {
+
+        for (Map.Entry<Integer, List<Battle>> battle : sessionBattles.entrySet()) {
             int xpearned = 0;
             AppendText("Session " + battle.getKey().toString() + ":", Color.CORNFLOWERBLUE, true);
             if (xpEntries.containsKey(battle.getKey())) {
                 xpearned = xpEntries.get(battle.getKey()).stream().collect(Collectors.summingInt(x -> x.getXP()));
                 for (XPEvent xp : xpEntries.get(battle.getKey())) {
-                    AppendText("Earned " + Integer.toString(xp.getXP()) + " for " + xp.getEvent(), Color.CORNFLOWERBLUE, true);
+                    AppendText("Earned " + numberFormat.format(xp.getXP()) + " for " + xp.getEvent(), Color.CORNFLOWERBLUE, true);
                 }
             }
 
 //            AppendText("Monsters killed: " + battle.getValue().stream().collect(Collectors.summingInt(x -> x.getDeadMonsters())).toString(), Color.CORNFLOWERBLUE, true);
 //            AppendText("Monsters not killed (XP Given): " + battle.getValue().stream().collect(Collectors.summingInt(x -> x.getPersuadedMonsters())).toString(), Color.CORNFLOWERBLUE, true);
 //            AppendText("Monsters not killed (no XP Given): " + battle.getValue().stream().collect(Collectors.summingInt(x -> x.getUntouchedMonsters())).toString(), Color.CORNFLOWERBLUE, true);
-            AppendText("XP Earned: " + Integer.toString(xpearned), Color.CORNFLOWERBLUE, true);
+            AppendText("XP Earned: " + numberFormat.format(xpearned), Color.CORNFLOWERBLUE, true);
             AppendText("", Color.CORNFLOWERBLUE, true);
         }
 
         AppendText("", Color.CORNFLOWERBLUE, true);
         AppendText("Monsters killed:", Color.CORNFLOWERBLUE, true);
 
-        List<XPEvent> allevents = xpEntries.values().stream().flatMap(List::stream).collect(Collectors.toList());
-        //Map<String, Integer> monsterKills = allevents.stream().collect(Collectors.groupingBy(XPEvent::getMonster,Collectors.counting()));
-
+        for (Map.Entry<String, Long> kill : monsterKills.entrySet()) {
+            AppendText("Earned XP for " + numberFormat.format(kill.getValue()) + " " + kill.getKey(), Color.CORNFLOWERBLUE, true);
+        }
 //        List<BattleMonster> kills = new ArrayList<>();
 //        for (Battle battle : battles) {
 //            kills.addAll(battle.getMonsters());
@@ -148,6 +162,7 @@ public class CampaignStatsController extends BaseForm implements Initializable {
         }
 
         text1.setFill(color);
+        text1.setFont(Font.font("Helvetica", FontWeight.NORMAL, 18));
         Output.getChildren().add(text1);
 
 //            var box = richTextBox1;
